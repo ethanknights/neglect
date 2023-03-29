f833f8fa690e9998f4b559e7f5ecc73820a93977
ethanknights <ethanknights@hotmail.co.uk>
2023-03-29 15:14:44 +0100

    refactoring condition names

--- convert2long_experiment-2.py
+++ convert2long_experiment-2.py
@@ -20,22 +20,31 @@ df = df.reset_index()
 tmp = []
 for idx, _ in enumerate(df['Trial Name']):
     tmp.append(df['Trial Name'][idx][0:4])
+    # edge case
     if tmp[idx] == 'DA_2':
         tmp[idx] = 'DA'
 df['subjName'] = tmp
+del tmp
 
-# Add cond Names
-tmp_condition = [] # UNI_LH / UNI_RH / BI_CON / BI_INC
-tmp_hand = [] # LH / RH
-tmp_target = [] # Far / Near
-for idx, _ in enumerate(df['Trial Name']):
+# Assign condition / hand / target names
+tmp_condition = []  # UNI_LH / UNI_RH / BI_CON / BI_INC
+tmp_hand = []  # LH / RH
+tmp_target = []  # Far / Near
+for idx, _ in enumerate(df['Trial Name']):  # idx=0; _ = df['Trial Name'][idx]
+
+    # init
+    tmp_condition.append('NaN')
+    tmp_hand.append('NaN')
+    tmp_target.append('NaN')
+
     if 'LH' in _:
-        tmp_condition.append('UNI')
-        tmp_hand.append('LH')
-        if df['target'][idx] == 1:
-            tmp_target.append('Far')
-        elif df['target'][idx] == 2:
-            tmp_target.append('Near')
+        tmp_condition[idx] = 'UNI'
+        tmp_hand[idx] = 'LH'
+        if '1' in df['target'][idx]:
+            tmp_target[idx] = 'Far'
+        elif '2' in df['target'][idx]:
+            tmp_target[idx] = 'Near'
+
     elif 'RH' in _:
         tmp_condition.append('UNI')
         tmp_hand.append('RH')
@@ -72,9 +81,7 @@ for idx, _ in enumerate(df['Trial Name']):
             tmp_hand.append('LH')
             tmp_target.append('Near')
     else:
-        tmp_condition.append('NaN')
-        tmp_hand.append('NaN')
-        tmp_target.append('NaN')
+
 df['condition_name'] = tmp_condition
 df['hand_name']      = tmp_hand
 df['target_name']    = tmp_target
