07510f3eebf957c0d54d4dc3c0e4dc55ed121dfc
ethanknights <ethanknights@hotmail.co.uk>
2023-03-29 15:19:07 +0100

    front-append subjName column

--- convert2long_experiment-2.py
+++ convert2long_experiment-2.py
@@ -16,15 +16,17 @@ df.columns
 df = df.drop(df.filter(regex='Unnamed:').columns, axis=1) # df.drop(columns=['Unnamed: 27', 'Unnamed: 30'])  # DDrop some specific cols
 df = df.reset_index()
 
-# Add subj Names
+# Front-append subjName column
 tmp = []
 for idx, _ in enumerate(df['Trial Name']):
     tmp.append(df['Trial Name'][idx][0:4])
     # edge case
     if tmp[idx] == 'DA_2':
         tmp[idx] = 'DA'
-df['subjName'] = tmp
+df.insert(0, 'subjName', tmp)
 del tmp
+df = df.drop('index', axis=1)
+
 
 # Assign condition / hand / target names
 tmp_condition = []  # UNI_LH / UNI_RH / BI_CON / BI_INC
