e88b9a6f905e627a669b1e80b22a60f165124673
ethanknights <ethanknights@hotmail.co.uk>
2023-03-30 18:36:43 +0100

    WIP: quick pointing error ggplot

--- R/tmp_EDA.R
+++ R/tmp_EDA.R
@@ -1,6 +1,23 @@
 # initial EDA
-grouped <- group_by(df, subjName, condition_name, hand_name, target_name)
-summary <- summarise(grouped, mean=mean(Pointing_Error), sd=sd(Pointing_Error))
+
 
 # How is Patient?
-tmp = summary[summary['subjName'] == 'EB',]
+#tmp = summary[summary['subjName'] == 'EB',]
+tmp = summary
+
+# gist plot
+p <- ggplot(data=df_big_summary, aes(x=full_condition_name, colour=patient_label)) +
+  geom_point(aes(y = mean)) + 
+               scale_x_discrete(guide = guide_axis(angle = 60))
+p
+
+
+
+# Reminder: Main singcar funcions:
+# BTD() which supersedes Deficit test i.e. TD().
+# e.g. Is LH error different than controls? 
+
+# BSDT() which allows Deficit Test using difference between two tasks. 
+# e.g. is difference of LH & RH different for patient vs. controls
+
+
