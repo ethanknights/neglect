3bf248fbf56bbabc1c7714feee86231149153796
ethanknights <ethanknights@hotmail.co.uk>
2023-04-02 22:52:11 +0100

    WIP: quick pointing coordinate plot

--- R/tmp_EDA.R
+++ R/tmp_EDA.R
@@ -12,6 +12,23 @@ p <- ggplot(data=df_big_summary, aes(x=full_condition_name, colour=patient_label
 p
 
 
+# Plot pointing coordinates
+## assert no missing coordinates (xls formulae...)
+tmp = df[which(is.na(df$Zerodx)),]
+tmp = df[which(is.na(df$Zerody)),] 
+# artificial shift for plotting
+df$Zerodx_shift[df$hand_name == 'LH'] = df$Zerodx[df$hand_name == 'LH'] - 200
+df$Zerodx_shift[df$hand_name == 'RH'] = df$Zerodx[df$hand_name == 'RH'] + 200
+
+
+p <- ggplot(data=df, aes(colour=patient_label)) +
+  geom_point(aes(x = Zerodx_shift, y = Zerody)) + 
+  scale_x_discrete(guide = guide_axis(angle = 60))
+p
+p + facet_grid(cols = vars(full_condition_name))
+
+
+
 
 # Reminder: Main singcar funcions:
 # BTD() which supersedes Deficit test i.e. TD().
