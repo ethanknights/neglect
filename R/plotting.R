bf3d4222e5fa4fdc3b57d91b67ef30acfa3c557d
ethanknights <ethanknights@hotmail.co.uk>
2024-02-25 19:48:57 +0000

    plot with consistent method as peripheral experiment where patient geom is top layer

--- R/plotting.R
+++ R/plotting.R
@@ -1,24 +1,30 @@
 # Produce & write a point plot with a gist of the results
 source('calculate_bimanual_cost.R')
 
-# Rearrange df rows so patient at bottom, for ggplot order
-df_summary <- df_summary %>%
-  arrange(factor(patient_label, levels = c("Control", "Patient")), subjName)
+do_line_plot <- function(df_summary, descript_str, y_lim_lo = NULL, y_lim_hi = NULL) {
 
-do_line_plot <- function(df_summary, descript_str) {
-
-  p <- ggplot(data = df_summary, aes(x = full_condition_name, y = mean, colour = patient_label, group = subjName)) +
-    geom_point(size = 4, alpha = 0.8, shape = 21, aes(fill = patient_label)) +
-    geom_point(size = 4, alpha = 0.8, shape = 21, colour = "black", stroke = 0.6) +
-    geom_line(size = 0.8, alpha = 0.8, aes(linetype = patient_label)) +
-    scale_color_manual(values = c("magenta", "cyan")) +
-    scale_fill_manual(values = c("magenta", "cyan")) +
-    scale_linetype_manual(values = c("solid", "dashed")) +
+  p <- ggplot() +
+    geom_line(data = df_summary[df_summary$patient_label == "Control", ], 
+              aes(x = full_condition_name, y = mean, linetype = patient_label, group = subjName),
+              size = 0.8, alpha = 0.8, color = "cyan") +
+    geom_line(data = df_summary[df_summary$patient_label == "Patient", ], 
+              aes(x = full_condition_name, y = mean, linetype = patient_label, group = subjName),
+              size = 0.8, alpha = 0.8, color = "magenta") +
+    geom_point(data = df_summary[df_summary$patient_label == "Control", ], 
+               aes(x = full_condition_name, y = mean, colour = patient_label, group = subjName),
+               size = 4, alpha = 0.8, shape = 21, fill = "cyan", color = "black") +
+    geom_point(data = df_summary[df_summary$patient_label == "Patient", ], 
+               aes(x = full_condition_name, y = mean, colour = patient_label, group = subjName),
+               size = 4, alpha = 0.8, shape = 21, fill = "magenta", color = "black") +
+    scale_linetype_manual(values = c("dashed", "solid")) +
     scale_x_discrete(guide = guide_axis(angle = 60)) +
     theme(panel.background = element_blank(), panel.grid.major = element_blank(),
           panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"), 
-          legend.position = "none")
-
+          legend.position = "none"); p
+    if (!is.null(y_lim_lo) && !is.null(y_lim_hi)) {
+      p <- p + scale_y_continuous(limits = c(y_lim_lo, y_lim_hi))
+    }
+  
   ggsave(filename = file.path(outImageDir, paste0('lineplot', descript_str, '.png')),
          plot = p,
          width = 15,  #width = cmwidth,
@@ -53,19 +59,19 @@ p <- p + labs(title = 'mean_PV by Condition and Patient'); p
 descript_str_DV = 'mean_Pointing_Error' # Modify me only!
 curr_df_summary <- df_summary %>% select(subjName, patient_label, full_condition_name, mean = descript_str_DV)
 curr_df_summary <- calculate_bimanual_cost(curr_df_summary)
-p <- do_line_plot(curr_df_summary, paste0(descript_str_DV, '_', 'BI_cost')); p
+p <- do_line_plot(curr_df_summary, paste0(descript_str_DV, '_', 'BI_cost'), 0, 10); p
 p <- p + labs(title = 'mean_Pointing_Error BI COST by Condition and Patient'); p
 
 descript_str_DV = 'mean_reaction.time' # Modify me only!
 curr_df_summary <- df_summary %>% select(subjName, patient_label, full_condition_name, mean = descript_str_DV)
 curr_df_summary <- calculate_bimanual_cost(curr_df_summary)
-p <- do_line_plot(curr_df_summary, paste0(descript_str_DV, '_', 'BI_cost')); p
+p <- do_line_plot(curr_df_summary, paste0(descript_str_DV, '_', 'BI_cost'), -400, 400); p
 p <- p + labs(title = 'mean_RT BI COST by Condition and Patient'); p
 
 descript_str_DV = 'mean_movement.time' # Modify me only!
 curr_df_summary <- df_summary %>% select(subjName, patient_label, full_condition_name, mean = descript_str_DV)
 curr_df_summary <- calculate_bimanual_cost(curr_df_summary)
-p <- do_line_plot(curr_df_summary, paste0(descript_str_DV, '_', 'BI_cost')); p
+p <- do_line_plot(curr_df_summary, paste0(descript_str_DV, '_', 'BI_cost'), -400, 400); p
 p <- p + labs(title = 'mean_MT BI COST by Condition and Patient'); p
 
 descript_str_DV = 'mean_PV' # Modify me only!
